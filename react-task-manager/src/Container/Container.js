92ffa84470a5d95d175f7d056ed386d071179c70
Jhuanderson <jjmaci16@g.holycross.edu>
2019-02-09 14:13:27 -0500

    React_Component Launch

---
+++ react-task-manager/src/Container/Container.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './Container.css'
+
+class Container extends Component {
+  render() {
+    return (
+      <div className="container Container-marginTop Container-color">
+        <hr />
+        <footer>
+          <p>www.codehandbook.org</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Container;
